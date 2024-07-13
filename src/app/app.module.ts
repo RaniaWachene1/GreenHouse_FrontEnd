19fa5a87142761151202dc1ae9b211699c6ce217
Rania_Wachene <125664751+RaniaWachene1@users.noreply.github.com>
2024-07-13 16:12:58 +0200

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './FrontOffice/home/home.component';
+import { RegisterComponent } from './FrontOffice/register/register.component';
+import { LoginComponent } from './FrontOffice/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
